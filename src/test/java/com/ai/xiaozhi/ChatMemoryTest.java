36377c465963be7da00445bab89421adebbe80e0
smile <mad024039@gmail.com>
2025-05-14 22:44:26 +0800

    feat: 利用Aiservice实现对话记忆功能

---
+++ src/test/java/com/ai/xiaozhi/ChatMemoryTest.java
@@ -0,0 +1,39 @@
+package com.ai.xiaozhi;
+
+import com.ai.xiaozhi.assistant.Assistant;
+import com.ai.xiaozhi.assistant.MemoryChatAssistant;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.service.AiServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChatMemoryTest {
+  @Autowired
+  private QwenChatModel qwenChatModel;
+  @Test
+  public void testChatMemory() {
+    MessageWindowChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+    Assistant assistant = AiServices
+      .builder(Assistant.class)
+      .chatModel(qwenChatModel)
+      .chatMemory(chatMemory)
+      .build();
+    String answer1 = assistant.chat("解释一下java中的反射机制");
+    System.out.println(answer1);
+    String answer2 = assistant.chat("给出其经典的应用场景");
+    System.out.println(answer2);
+  }
+
+  @Autowired
+  private MemoryChatAssistant memoryChatAssistant;
+  @Test
+  public void testMemoryChatAssistant() {
+    String answer1 = memoryChatAssistant.chat("我是ElePhantom");
+    System.out.println(answer1);
+    String answer2 = memoryChatAssistant.chat("我是谁");
+    System.out.println(answer2);
+  }
+}
