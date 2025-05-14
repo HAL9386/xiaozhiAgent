d0c2d9c8d7260cd2e0ac78a33dc0fc1550129236
smile <mad024039@gmail.com>
2025-05-14 20:19:12 +0800

    test: 接入阿里百炼qwen-max

--- src/test/java/com/ai/xiaozhi/LLMTest.java
+++ src/test/java/com/ai/xiaozhi/LLMTest.java
@@ -1,12 +1,15 @@
 package com.ai.xiaozhi;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @SpringBootTest
 public class LLMTest {
   @Value("${openai.api.key}")
@@ -35,6 +38,14 @@ public class LLMTest {
   @Test
   public void testOllama() {
     String answer = ollamaChatModel.chat("你是谁");
+    System.out.println(answer);
+  }
+
+  @Autowired
+  private QwenChatModel qwenChatModel;
+  @Test
+  public void testQwen() {
+    String answer = qwenChatModel.chat("你是谁");
     System.out.println(answer);
   }
 }
