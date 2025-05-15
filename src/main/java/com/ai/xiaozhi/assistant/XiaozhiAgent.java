5845af0e81ae41e462acd648a3fb3038798dba31
smile <mad024039@gmail.com>
2025-05-15 23:29:00 +0800

    feat: 配置小智Agent
    利用AiService新建一个Agent，系统提示词从文件导入。
    chatMemoryProvider在config中配置

---
+++ src/main/java/com/ai/xiaozhi/assistant/XiaozhiAgent.java
@@ -0,0 +1,17 @@
+package com.ai.xiaozhi.assistant;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+
+@AiService(
+  wiringMode = AiServiceWiringMode.EXPLICIT,
+  chatModel = "qwenChatModel",
+  chatMemoryProvider = "chatMemoryProviderXiaozhi"
+)
+public interface XiaozhiAgent {
+  @SystemMessage(fromResource = "XiaozhiSystemPrompt.txt")
+  String chat(@MemoryId int memoryId, @UserMessage String userMessage);
+}
